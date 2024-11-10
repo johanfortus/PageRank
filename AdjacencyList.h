4b94b7ef819a24907a9986da711286422f80ccd8
johanfortus <fortusjohan@gmail.com>
2024-11-09 20:38:38 -0500

    how to make pair

--- AdjacencyList.h
+++ AdjacencyList.h
@@ -3,13 +3,25 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <utility>
 #include <cmath>
 using namespace std;
 
+// how to make a pair
+pair<string, double> productOne ("tomatoes",2.30);
+
+// or
+
+pair<string, double> productTwo = {"tomatoes",2.30};
 
 class AdjacencyList {
 
     // Think about what member variables you need to initialize
+    int pageID;
+    vector<pair<int, int>> adjacents;
+
+    // map<pageId, adjacents<pageId, weight>
+    map<int, vector<pair<int, int>>> adj;
 
 public:
 
